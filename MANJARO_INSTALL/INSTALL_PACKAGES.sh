eb6039f68fa3e3595da1a8a46bde1bffd32c4204
json <jason.carter.martin@gmail.com>
2020-04-07 13:24:07 -0700

    Update INSTALL script for Manjaro

---
+++ MANJARO_INSTALL/INSTALL_PACKAGES.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo pacman -Syu # Download & update mirrors
+sudo pacman -S yay # Install YAY package manager
+yay -S base-devel # Install base development packages, which are helpful for installing other packages
+yay -S "$(cat INSTALL_PACKAGES.txt | xargs)" # Install list of all packages
--- MANJARO_INSTALL/install_app_list.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-yay -S base-devel
-yay -S $(cat app_list.txt | xargs)
-
