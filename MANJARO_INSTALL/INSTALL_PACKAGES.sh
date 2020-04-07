120d520b26f3c08d08417a283f95d7eb90690736
json <jason.carter.martin@gmail.com>
2020-04-07 15:52:50 -0700

    Fix install script bug

--- MANJARO_INSTALL/INSTALL_PACKAGES.sh
+++ MANJARO_INSTALL/INSTALL_PACKAGES.sh
@@ -2,4 +2,4 @@
 sudo pacman -Syu # Download & update mirrors
 sudo pacman -S yay # Install YAY package manager
 yay -S base-devel # Install base development packages, which are helpful for installing other packages
-yay -S "$(cat INSTALL_PACKAGES.txt | xargs)" # Install list of all packages
+yay -S $(cat INSTALL_PACKAGES.txt | xargs) # Install list of all packages
